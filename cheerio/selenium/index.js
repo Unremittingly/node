b078aa8b604d305f2e10840952c72fe4c1b9ab92
zhangqiang <13608164790zq93>
2019-01-17 14:49:25 +0800

    init

--- cheerio/selenium/index.js
+++ cheerio/selenium/index.js
@@ -5,6 +5,8 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const {Options} = require('selenium-webdriver/chrome');
 
+const webdriver = require('selenium-webdriver');
+
 
 
 
@@ -13,14 +15,34 @@ var driver = new Builder()
     .setChromeOptions(new Options().excludeSwitches(["ignore-certificate-errors", 'user-data-dir="C:\\Users\\qiang\\AppData\\Local\\Google\\Chrome\\User Data"']))
     .build();
 
-driver.get('http://127.0.0.1:7777/')
+driver.get('http://www.baidu.com')
     .then(function () {
         console.log('2222');
    }).catch(function (e) {
     console.log('2222', e);
 });
 
-let select =  driver.find(By.id('select'));
-let span = driver.findElement(By.id('span'));
-console.log('select',select,span);
+
+
+let span = driver.findElement(By.id('u1'));
+let tj_trnews = driver.findElement(By.name('tj_trnews'));
+let searchIpt = driver.findElement(By.id('kw'));
+let searchBtn = driver.findElement(By.id('su'));
+let searchStr = 'aa';
+searchIpt.sendKeys(searchStr);
+
+// driver.get('http://127.0.0.1:7777').then(function () {
+//     let select = driver.findElement(By.xpath('option'));
+//     console.log('select',select.length);
+// });
+
+//模拟点击事件
+// tj_trnews.click(function () {
+//
+// });
+//模拟搜索点击事件
+searchBtn.click(function () {
+
+});
+
 
