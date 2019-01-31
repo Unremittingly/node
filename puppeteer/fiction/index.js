58266036125d66f0aaa1226699ef4dc2dd79236f
zhangqiang <13608164790zq93>
2019-01-31 11:12:51 +0800

    生成二维码

--- puppeteer/fiction/index.js
+++ puppeteer/fiction/index.js
@@ -10,6 +10,8 @@ const cheerio = require('cheerio');
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
 
+    let success = [];
+
 
     getDatas();
 
@@ -22,11 +24,14 @@ const cheerio = require('cheerio');
         // for (let i = start; i < start + 10; i++) {
         //     await getData1(i);
         // }
-        //八一新中网
-        for (let i = 56; i < 100; i++) {
-            await getData(i);
+        //八一新中网   94位置  白屏啦
+        for (let i = 250; i < 300; i++) {
+            let isSuccess = await getData(i);
+            if (!isSuccess) {
+                break;
+            }
         }
-
+        console.log('success', success);
         browser.close();
 
     }
@@ -35,72 +40,109 @@ const cheerio = require('cheerio');
     async function getData(num) {
         //八一新中网
         let url = 'https://www.81xzw.com/book/151103/';
-        let result = await page.goto(url + num + '.html');
+        let time = new Date().getTime() / 1000;
+        try {
+            let result = await page.goto(url + num + '.html', {
+                waitUntil: 'networkidle0',
+                timeout: 10000
+            });
+        } catch (e) {
+            console.log('error', e);
+            return false;
+        }
 
-        let cur = await page.evaluate(function () {
-            let title = document.getElementsByTagName('h1')[0].innerText;
-            // text=text.replace(/\n/g,"<br/>");
-            let num = title.match(/\((.+?)\)/g);
-            if (num && num.length > 0) {
-                num = num[0].match(/\/(\S*)/)[1].replace(')', '');
-                return {
-                    title: title.replace(/\((.+?)\)/g, ''),
-                    num: num
+        let n_time = new Date().getTime() / 1000;
+        console.log('n_time', n_time - time, num);
+        if (n_time - time > 10) {
+            console.log('链接超时，error');
+            return false;
+        }
+        let cur = null;
+        try {
+             cur = await page.evaluate(function () {
+                let d_tit = document.getElementsByTagName('h1')[0];
+                let title = d_tit ? d_tit.innerText : '';
+                // text=text.replace(/\n/g,"<br/>");
+                let num = title.match(/\((.+?)\)/g);
+                if (num && num.length > 0) {
+                    num = num[0].match(/\/(\S*)/)[1].replace(')', '');
+                    return {
+                        title: title.replace(/\((.+?)\)/g, ''),
+                        num: num
+                    }
+                } else {
+                    return {
+                        title: title,
+                        num: 1
+                    };
                 }
-            } else {
-                return {
-                    title: title,
-                    num: 1
-                };
-            }
 
 
-        });
+            });
+
+        } catch (e) {
+            console.log('error', e);
+            return false;
+        }
         console.log('data_num', cur);
+        if(!cur.title){
+            return false;
+        }
         let data = {
             text: '',
         };
-        if (cur.num >= 1) {
-            for (let i = 0; i < cur.num; i++) {
-                await page.goto(url + num + '_' + i + '.html');
-                let data_i = await page.evaluate(function () {
+        try {
+
+
+            if (cur.num >= 1) {
+                for (let i = 0; i < cur.num; i++) {
+                    await page.goto(url + num + '_' + i + '.html');
+                    let data_i = await page.evaluate(function () {
+                        let d_tit = document.getElementsByTagName('h1')[0];
+                        let title = d_tit ? d_tit.innerText : "";
+                        let d_txt = document.getElementById('content');
+                        let text = d_txt ? d_txt.innerText : '';
+                        let to_nextpage = document.getElementsByClassName('to_nextpage')[0] ? document.getElementsByClassName('to_nextpage')[0].innerText : '';
+                        text = text.replace(to_nextpage + '', '').replace('-->>', '');
+                        // text=text.replace(/\n/g,"<br/>");
+                        return {
+                            title: title,
+                            text: text,
+                        }
+                    });
+                    // console.log('data_i',data_i);
+                    data.text += data_i.text;
+                }
+            } else {
+                data = await page.evaluate(function () {
                     let d_tit = document.getElementsByTagName('h1')[0];
-                    let title = d_tit ? d_tit.innerText : "";
+                    let title = d_tit ? d_tit.innerText : '';
                     let d_txt = document.getElementById('content');
                     let text = d_txt ? d_txt.innerText : '';
-                    let to_nextpage = document.getElementsByClassName('to_nextpage')[0] ? document.getElementsByClassName('to_nextpage')[0].innerText : '';
-                    text = text.replace(to_nextpage + '', '').replace('-->>', '');
+                    let to_nextpage = document.getElementsByClassName('to_nextpage').innerText;
+                    text = text.replace(to_nextpage + '', '');
                     // text=text.replace(/\n/g,"<br/>");
                     return {
                         title: title,
-                        text: text,
+                        text: text
                     }
                 });
-                // console.log('data_i',data_i);
-                data.text += data_i.text;
             }
-        } else {
-            data = await page.evaluate(function () {
-                let d_tit = document.getElementsByTagName('h1')[0];
-                let title = d_tit?d_tit.innerText:'';
-                let d_txt = document.getElementById('content');
-                let text = d_txt ? d_txt.innerText : '';
-                let to_nextpage = document.getElementsByClassName('to_nextpage').innerText;
-                text = text.replace(to_nextpage + '', '');
-                // text=text.replace(/\n/g,"<br/>");
-                return {
-                    title: title,
-                    text: text
-                }
-            });
-
+        } catch (e) {
+            console.log('errrorrrr', e);
+            return false;
         }
 
         data.title = cur.title;
         // console.log('data',data);
         if (data.text) {
-            writeTxtToFile(data.title + '\n\n' + data.text, './puppeteer/fiction/fr.txt');
+            if (data.title.indexOf('凡人修仙传') != -1) {
+                success.push(data.title);
+            }
+            writeTxtToFile('\n' + data.title + '\n\n' + data.text, './puppeteer/fiction/fr.txt');
         }
+
+        return true;
 
     }
 
