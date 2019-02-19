9117daaf221514dd7d8a979543b917dfeecd83b5
zhangqiang <13608164790zq93>
2019-02-19 14:39:00 +0800

    test

--- channel/index.js
+++ channel/index.js
@@ -12,8 +12,6 @@ function initChannel() {
     let regArr = [12,];
 
     function writeTxtToFile(str, url) {
-
-
         if (!getIsExist(url)) {
             //异步方法
             fs.writeFile(url, '初始化第一行', function (err) {
@@ -44,8 +42,8 @@ function initChannel() {
 }
 getChanel();
 function  getChanel() {
-    let arr = [70006,70007,70008,70009,70010,70011,70012,70013, 70014,70015,70016,70017,70018,70019,
-        26021,46021,56021,70028,82016,12007,12008,12009,12012,26043,70028];
+    let arr = [70006,70007,70008,70009,70010,70011,70012,70013,70014,70015,70016,70017,70018,70019,
+        26021,46021,56021,70028,82016,12007,12008,12009,12012,26043,70028,70033];
     let randoms = [];
     let length = arr.length;
     for (let i = 0; i < length; i++) {
